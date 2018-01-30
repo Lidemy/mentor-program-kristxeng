b752cc4fc340b8440b80b6a07bd8c73e0c314ec0
Kris <kristxeng@gmail.com>
2018-01-30 22:53:04 +0800

    hw2 patch

--- homeworks/week1/hw2.js
+++ homeworks/week1/hw2.js
@@ -1,7 +1,7 @@
 export const capitalize = (str) => {
   if( str.charCodeAt(0) >=97 &&str.charCodeAt(0)<=122){
   
-  str = String.fromCharCode(str.charCodeAt(0)-32) + str.substring(1)
+    str = String.fromCharCode(str.charCodeAt(0)-32) + str.substring(1)
   
   }
   return str;
