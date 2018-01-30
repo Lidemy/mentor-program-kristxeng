efed42d5388110abc39a86f2774c849d0c929bd5
Kris <kristxeng@gmail.com>
2018-01-30 20:12:22 +0800

    week1_hw1 added

--- homeworks/week1/hw1.js
+++ homeworks/week1/hw1.js
@@ -1,3 +1,13 @@
 export const stars = (n) => {
-  return;
+
+  var star=[]
+
+  for(var i=0; i<n; i++){
+    star[i] = i? star[i-1]+'*' : '*'
+    console.log(star[i])
+  }
+  
+  console.log(star)
+  
+  return star;
 }
