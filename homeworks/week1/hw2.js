22c549311f76e473d21db5d71c8108c98fd4d3c1
Kris <kristxeng@gmail.com>
2018-01-30 22:34:14 +0800

    hw2 finished!

--- homeworks/week1/hw2.js
+++ homeworks/week1/hw2.js
@@ -1,3 +1,8 @@
 export const capitalize = (str) => {
+  if( str.charCodeAt(0) >=97 &&str.charCodeAt(0)<=122){
+  
+  str = String.fromCharCode(str.charCodeAt(0)-32) + str.substring(1)
+  
+  }
   return str;
 }
--- homeworks/week1/hw2.test.js
+++ homeworks/week1/hw2.test.js
@@ -4,4 +4,13 @@ describe("hw2", () => {
   it("should return correct answer when str = nick", () => {
     expect(capitalize('nick')).toBe('Nick')
   })
+  it("should return correct answer when str = Adam", () => {
+    expect(capitalize('Adam')).toBe('Adam')
+  })
+  it("should return correct answer when str = 123abc", () => {
+    expect(capitalize('123abc')).toBe('123abc')
+  })
+  it("should return correct answer when str = @tom", () => {
+    expect(capitalize('@tom')).toBe('@tom')
+  })
 })
