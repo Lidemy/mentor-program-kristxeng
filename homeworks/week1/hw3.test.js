783450cfb175bef90732f8eb0b3f7a648d36b87d
Kris <kristxeng@gmail.com>
2018-01-31 00:08:03 +0800

    w1 hw3 upload

--- homeworks/week1/hw3.test.js
+++ homeworks/week1/hw3.test.js
@@ -4,4 +4,29 @@ describe("hw3", () => {
   it("should return correct answer when n = 1", () => {
     expect(isPrime(1)).toBe(false)
   })
+
+  it("should return correct answer when n = 1451", () => {
+    expect(isPrime(1451)).toBe(true)
+  })
+
+  it("should return correct answer when n = 491", () => {
+    expect(isPrime(491)).toBe(true)
+  })
+
+  it("should return correct answer when n = 1024", () => {
+    expect(isPrime(1024)).toBe(false)
+  })
+
+  it("should return correct answer when n = 2", () => {
+    expect(isPrime(2)).toBe(true)
+  })  
+
+  it("should return correct answer when n = 3", () => {
+    expect(isPrime(3)).toBe(true)
+  })
+
+  it("should return correct answer when n = 28", () => {
+    expect(isPrime(28)).toBe(false)
+  })
+  
 })
