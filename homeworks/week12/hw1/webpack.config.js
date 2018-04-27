96ad13b3acccbaf20ee094d28df8d4db25b09173
kristxeng <kristxeng@gmail.com>
2018-04-27 13:40:21 +0800

    w12hw1 upload

---
+++ homeworks/week12/hw1/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  module: {
+    rules: [{ 
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader:"babel-loader" }
+    ]
+  },
+  output:{
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+    
+  }
+}
